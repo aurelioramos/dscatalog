ecb381d4e11e0f328ec599879b4baafcbcfdd868
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-05-01 19:44:06 -0400

    Add unit tests for persist operation

--- backend/src/test/java/br/edu/ifms/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/br/edu/ifms/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,9 +1,14 @@
 package br.edu.ifms.dscatalog.repositories;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+
+import br.edu.ifms.dscatalog.entities.Product;
+import br.edu.ifms.dscatalog.tests.Factory;
 
 @DataJpaTest
 class ProductRepositoryTests {
@@ -11,12 +16,47 @@ class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+    long existingId;
+    long nonExistingId;
+    long countTotalProducts;
+
+    @BeforeEach
+    void setUp() {
+
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
     @Test
     void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
+
         repository.deleteById(existingId);
         var result = repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
 
+    @Test
+    void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
+
+    @Test
+    void saveShouldThrowRuntimeExceptionWhenObjectIsNull() {
+
+        Assertions.assertThrows(RuntimeException.class, () -> repository.save(null));
+    }
+
+    @Test
+    void saveShouldThrowInvalidDataAccessApiUsageExceptionWhenObjectIsNull() {
+
+        Assertions.assertThrowsExactly(InvalidDataAccessApiUsageException.class, () -> repository.save(null));
+    }
 }
---
+++ backend/src/test/java/br/edu/ifms/dscatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package br.edu.ifms.dscatalog.tests;
+
+import java.time.Instant;
+
+import br.edu.ifms.dscatalog.dto.ProductDTO;
+import br.edu.ifms.dscatalog.entities.Category;
+import br.edu.ifms.dscatalog.entities.Product;
+
+public class Factory {
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png",
+                Instant.parse("1970-01-01T00:00:00Z"));
+        product.getCategories().add(new Category(2L, "Eletronics"));
+        return product;
+    }
+
+    public static ProductDTO creaProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
